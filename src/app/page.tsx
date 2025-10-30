140f23b5ee31be31c97e090e2e110d56b9d76a66
Daniel Fang <daniel.bb0321@gmail.com>
2025-10-29 18:06:37 -0700

    latest

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,128 @@
 import Image from "next/image";
+import { Pixelify_Sans } from 'next/font/google';
+import "@/styles/landing_page_new.css";
+
+const pixelifySans = Pixelify_Sans({
+  weight: ['400', '700'],
+});
 
 export default function Home() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+    <>
+      <main>
+        {/* visuals */}
+        <div className="visuals">
+          <div id="clouds">
             <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
+              className="cloud"
+              src="/images/thumbnail_cloud.png"
+              alt="Cloud Thumbnail"
+              width={200}
+              height={100}
             />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+            <Image
+              className="cloud"
+              src="/images/thumbnail_cloud.png"
+              alt="Cloud Thumbnail"
+              width={200}
+              height={100}
+            />
+            <Image
+              className="cloud"
+              src="/images/thumbnail_cloud.png"
+              alt="Cloud Thumbnail"
+              width={200}
+              height={100}
+            />
+          </div>
+
+          {/* tree */}
+          <Image
+            className="tree_animation"
+            src="/images/tree_animation2.gif"
+            alt="landing page design"
+            width={800}
+            height={600}
+            unoptimized
+          />
+
+          {/* sun */}
+          <div className="sun" aria-hidden="true">
+            <div className="sun-rays">
+              <div className="ray r1"></div>
+              <div className="ray r2"></div>
+              <div className="ray r3"></div>
+              <div className="ray r4"></div>
+              <div className="ray r5"></div>
+              <div className="ray r6"></div>
+              <div className="ray r7"></div>
+              <div className="ray r8"></div>
+              <div className="ray r9"></div>
+              <div className="ray r10"></div>
+              <div className="ray r11"></div>
+              <div className="ray r12"></div>
+              <div className="ray r13"></div>
+              <div className="ray r14"></div>
+              <div className="ray r15"></div>
+              <div className="ray r16"></div>
+              <div className="ray r17"></div>
+              <div className="ray r18"></div>
+            </div>
+          </div>
+
+          {/* speech bubble */}
+          <div className="bubble-wrap">
+            <div className="water-droplet-words">
+              <h4>did you know?</h4>
+            </div>
+
+            <Image
+              className="speech_bubble"
+              src="/images/speech bubble.png"
+              alt="Speech Bubble"
+              width={300}
+              height={200}
+            />
+          </div>
+
+          {/* arrow */}
+          <Image
+            className="arrow swing-notilt"
+            src="/images/arrow2.png"
+            alt="Arrow"
+            width={100}
+            height={100}
+          />
+        </div>
+
+        {/* textcontent */}
+        <div className={`additional-content ${pixelifySans.className}`}>
+          <h2 className="stacked-words">
+            <span>This</span> <span>Building</span> <span>Saves</span>
+          </h2>
+          <h3>WATER</h3>
+          <p>more info coming soon</p>
         </div>
       </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+
+
+      {/* <div className="brand-row">
+        <Image
+          className="logo"
+          src="/images/W-Logo_White.png"
+          alt="UW Logo"
+          width={100}
+          height={100}
+        />
+        <h1 className="brand-title">x</h1>
+        <Image
+          className="ewb-logo"
+          src="/images/thumbnail_Square Logo - UWClear.png"
+          alt="EWB Logo"
+          width={100}
+          height={100}
+        />
+      </div> */}
+    </>
   );
 }
